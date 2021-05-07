124ba374bd2732c2afd1ae8d08776a88a34bd8b6
CSWILIK <cswilik@gmail.com>
2021-05-07 13:10:08 -0400

    adjusting api link

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -6,7 +6,7 @@ function SearchBar({setMovies, movies}) {
     
 
     useEffect(() => {
-        fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${search}`)
+        fetch(`https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${search}`)
         .then(r => r.json())
         .then(data => handleData(data.Search)
         )}, [search])
